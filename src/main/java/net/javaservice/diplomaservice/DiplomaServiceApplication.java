29243c131d27df4edb3fe99bab6dd783abdc5958
Aleksander Kurys <112685770+AleksanderKurys@users.noreply.github.com>
2023-04-19 11:36:31 +0300

    Init project

---
+++ src/main/java/net/javaservice/diplomaservice/DiplomaServiceApplication.java
@@ -0,0 +1,14 @@
+package net.javaservice.diplomaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaServiceApplication {
+
+	public static void main(String[] args) {
+		System.out.print("aboba");
+		SpringApplication.run(DiplomaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaservice/diplomaservice/DiplomaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaservice.diplomaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
